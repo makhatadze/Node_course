05cb744cffbbc3be5210b1f6ded7b50cc0ba9bb1
makhatadze <vitomaxatadze@gmail.com>
2019-10-25 18:05:39 +0400

    Change all function by arrow function

--- notes-app/app.js
+++ notes-app/app.js
@@ -22,7 +22,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv){
+    handler(argv){
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -39,7 +39,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv){
+    handler(argv){
        notes.removeNote(argv.title)
     }
 }) 
@@ -49,14 +49,14 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function(){
+    handler(){
         console.log('Read the note')
     }
 })
 yargs.command({
     command: 'list',
     describe: 'List a note',
-    handler: function(){
+    handler(){
         console.log('List the note')
     }
 })
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const getNotes = function() {
+const getNotes = () => {
 
     return 'asdasd'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
         const notes = loadNotes()
-        const duplicateNotes = notes.filter(function (note) {
-            return note.title ===title
-        })
+        // const duplicateNotes = notes.filter(function (note) {
+        //     return note.title ===title
+        // })
+        const duplicateNotes = notes.filter((note) => note.title === title)
         if (duplicateNotes.length === 0) {
             notes.push({
                 title: title,
@@ -21,11 +22,9 @@ const addNote = function (title, body) {
             console.log('Note title taken')
         }
 }
-const removeNote = function (title){
+const removeNote = ((title) =>{
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title )
 
     if (notes.length > notesToKeep.length) { 
         console.log(chalk.green.inverse('Note removed!'))
@@ -34,13 +33,14 @@ const removeNote = function (title){
         console.log(chalk.red.inverse('No note found!'))
     }
 
-}
-const saveNotes = function (notes) {
+})
+
+const saveNotes =  ((notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json',dataJSON)
 
-}
-const loadNotes = function (){
+})
+const loadNotes =  () =>{
 
     try{
         const dataBuffer = fs.readFileSync('notes.json')
